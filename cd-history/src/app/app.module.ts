a3c8a337e56f567e5d85cf675b5dbc2d51236d0c
beckham <beckham.lam@mail.mcgill.ca>
2020-03-07 00:10:36 -0500

    layout(main):

--- cd-history/src/app/app.module.ts
+++ cd-history/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MainComponent } from './dashboard/main/main.component';
+import {MatIconModule} from '@angular/material/icon';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
