90e912df1c7337c7a3d92c2d7abb72b68a6cc6f9
beckham <beckham.lam@mail.mcgill.ca>
2020-03-08 20:18:06 -0400

    added new entries:

--- cd-history/src/app/dashboard/main/main.component.ts
+++ cd-history/src/app/dashboard/main/main.component.ts
@@ -11,8 +11,8 @@ export class MainComponent implements OnInit {
   constructor() { }
 
   entries: TimelineModel[] = [
-    {title: 'created', image: '', description: 'this is desc', year: '1992'},
-    {title: 'created', image: '', description: 'this is desc', year: '1993'}
+    {title: 'created', image: '../../assets/james-jebbia.jpg', description: 'Supreme is founded in New York city by James Jebbia', year: '1994'},
+    {title: 'created', image: '../../assets/travis-tee.jpg', description: 'Travis Bickle - First product Supreme released', year: '1994'}
   ] ;
 
   ngOnInit() {
