f0dbbd2907de0127d495cec475495c156b903920
beckham <beckham.lam@mail.mcgill.ca>
2020-03-10 22:00:52 -0400

    feat(difficulty page)

--- cd-history/src/app/app.module.ts
+++ cd-history/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './dashboard/main/main.component';
 import { TimelineComponent } from './dashboard/timeline/timeline.component';
 import { LanguageGridComponent } from './dashboard/language-grid/language-grid.component';
+import { ProgrammingDifficultyComponent } from './dashboard/programming-difficulty/programming-difficulty.component';
 // import {MatIconModule} from '@angular/material/icon';
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     TimelineComponent,
-    LanguageGridComponent
+    LanguageGridComponent,
+    ProgrammingDifficultyComponent
   ],
   imports: [
     BrowserModule,
---
+++ cd-history/src/app/dashboard/programming-difficulty/programming-difficulty.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-programming-difficulty',
+  templateUrl: './programming-difficulty.component.html',
+  styleUrls: ['./programming-difficulty.component.scss']
+})
+export class ProgrammingDifficultyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
